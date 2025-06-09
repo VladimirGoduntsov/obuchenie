05a24d090d16c14c08cbd4a956e153514584489f
Vladimir <02stlk@gmail.com>
2025-06-09 22:00:18 +0300

    task3: Внесены изменения

--- task3/data.go
+++ task3/data.go
@@ -6,15 +6,17 @@ import (
 )
 
 func main() {
-	data := "Hello World"
-	//data2 := []rune(data)
-	//fmt.Println(data2)
-	fmt.Println(data)
-	
-	for _, v := range data {
-		fmt.Println(string(v+3))
-	
 
-	}
+	data := "Vova"
+	data2 := Cesar(data, 3)
+	fmt.Println(data2)
 	
+}
+
+func Cesar(data string, kluch int) string {
+	result := make([]rune, len([]rune(data)))
+	for i, v := range data {
+	result[i] = v+rune(kluch)
+	}
+	return string(result)
 }
